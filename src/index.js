f08aa6d81487ab618318fbe09d853b247701d05c
Gulfidan34 <gulfidanozturk34@gmail..com>
2023-07-01 09:16:25 -0700

    gh pags setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import { ToastContainer } from "react-toastify";
@@ -10,10 +11,10 @@ import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
       <ToastContainer />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
